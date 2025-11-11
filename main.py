ec1ae9c4bb50debd90b70481d44804818445d07c
성시우 <siwoo01231324@gmail.com>
2025-11-11 15:42:44 +0900

    Feat:Auth 로그아웃, 세션 적용

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel
 # 암호화 관련 모듈 
 import bcrypt
+# 세션 관리
+from starlette.middleware.sessions import SessionMiddleware
 # ---------------------------------------------------------------------------------------------------------
 #
 #   전역변수 : 앱, 템플릿, 정적폴더, ORM 설정
@@ -17,6 +19,13 @@ import bcrypt
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static",StaticFiles(directory="static"),name="static")
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key='wsldfjwefjo823rlkwefnlkefnles', # 추후 외부에서 관리
+    session_cookie='memo_session', # 세션 쿠키의 이름
+    max_age=1*60*60*24 
+)
 
 # ORM 작업 (전역 변수 위치)
 # 데이터베이스 연동 URL 구성 = 프로토콜://아이디:비밀번호@IP/데이터베이스명
@@ -162,14 +171,15 @@ async def signup(user:UserInsert, db_conn : Session = Depends(get_connection)):
 
 # 로그인 -> 사용자 정보(비밀번호) 때문에 post
 @app.post("/signin")
-async def signin(login_data:UserLogin, db_conn : Session = Depends(get_connection)):
+async def signin(req:Request,login_data:UserLogin, db_conn : Session = Depends(get_connection)):
     # 1. 로그인 데이터중 고유한 값 -> username 존재하는지 체크
     target_user = db_conn.query(User).filter(User.username == login_data.username).first()
     # 2. 해당 유저가 존재하고, 비밀번호가 해싱된 비밀번호와 대조시 일치하면 
     if target_user and check_vaild_password(login_data.password, target_user.hashed_password):
         # 2-1. 둘 다 참이면 => 고객 ok => 세션 생성
+        req.session["username"] = login_data.username
         # 2-2. 더미로 응답
-            return {"msg":"로그인 성공"}
+        return {"msg":"로그인 성공"}
     return {"msg":"로그인 실패"}
 
 
@@ -177,7 +187,9 @@ async def signin(login_data:UserLogin, db_conn : Session = Depends(get_connectio
 
 # 로그아웃  -> 반드시 js로 처리한다 -> 브라우저 주소창에 넣어서 구동 x -> get으로 로그아웃 시도하면 안된다.
 @app.post("/logout")
-async def logout():
+async def logout(req:Request):
+    # 세션 제거
+    req.session.pop("username", None)
     pass
 
 
